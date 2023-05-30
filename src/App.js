c7a9c7299d7e478f8beb07738dba6a77e880bc4d
Mouleas <mouleaswar2001@outlook.com>
2023-05-30 08:08:47 +0530

    [ADD] Calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  let [num1, setNum1] = useState(''); 
+  let [num2, setNum2] = useState(''); 
+  let [output, setOutput] = useState('Output'); 
+
+
+  const handleNum1 = (event) => {
+    setNum1(event.target.value);
+  }
+  const handleNum2 = (event) => {
+    setNum2(event.target.value);
+  }
+
+  function handleOps(ops){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if (ops === "+"){
+      setOutput(num1+num2);
+    }
+    else if (ops === "-"){
+      setOutput(num1-num2);
+    }
+    else if (ops === "*"){
+      setOutput(num1*num2);
+    }
+    else {
+      setOutput(num1/num2);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="container">
+        <div className='header'></div>
+        <input type='number' placeholder='Type a number' onChange={handleNum1}></input>
+        <input type='number' placeholder='Type a number' onChange={handleNum2}></input>
+
+        <div className='ops'>
+          <button className='button' onClick={() => handleOps("+")}>➕</button>
+          <button className='button' onClick={() => handleOps("-")}>➖</button>
+          <button className='button' onClick={() => handleOps("*")}>✖</button>
+          <button className='button' onClick={() => handleOps("/")}>➗</button>
+        </div>
+
+        <div className='output'>
+          {output}
+        </div>
+      </div>
     </div>
   );
 }
